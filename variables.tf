cfcf9f2976e52e61f9720baf8767d550f71a765c
kawsark <kawsar@hashicorp.com>
2020-04-14 16:10:56 -0400

    updated webapp

--- variables.tf
+++ variables.tf
@@ -35,14 +35,15 @@ variable "subnetwork" {
 
 variable "startup_script" {
   default = <<EOF
-#! /bin/bash
-apt-get update
-apt-get install -y apache2
-cat <<EOF > /var/www/html/index.html
-<html><body><h1>Hello World</h1>
-<p>This page was created from a simple startup script!</p>
-</body></html>
+#This is a startup-script that will install an example application on the provisioned server
+#!/bin/bash
+sudo apt-get install git -y
+cd /tmp
+git clone https://github.com/hashicorp/demo-terraform-101.git
+cp -r demo-terraform-101/assets .
+cd assets
+sudo chmod +x ./setup-web.sh
+sudo ./setup-web.sh
 EOF
-
 }
 
